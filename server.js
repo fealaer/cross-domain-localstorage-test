8a103a8bc888e669c27c7722f5bc6f9ec6e88dc8
Andrey Pushkarev <fealaer@gmail.com>
2015-11-28 14:59:01 +0800

    Added simple express server -- needed to serve static files

---
+++ server.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  var express = require('express')
+    , app = express();
+
+  app.set('port', (process.env.PORT || 5000));
+  app.use(express.static('public'));
+
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
+})();
